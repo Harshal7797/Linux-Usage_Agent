477ba00de1024c89edfed3ab8b89da684a64eb2e
harshal-jrvs <51926543+harshal-jrvs@users.noreply.github.com>
2019-07-10 09:55:44 -0400

    Update sql_solution.sql

--- sql_solution.sql
+++ sql_solution.sql
@@ -1,13 +1,8 @@
 SELECT cpu_number, id AS host_id, total_mem from host_info ORDER BY cpu_number, total_mem DESC;
-
-
-
-
-
 SELECT  x.hostid,
-		x.hostname,
-	   (x.total_mem / 1000) AS total_memory,
- avg(used_memory) over(partition by interval) AS used_memory_percentage
+	x.hostname,
+	(x.total_mem / 1000) AS total_memory,
+	avg(used_memory) over(partition by interval) AS used_memory_percentage
 FROM
 	(SELECT u.host_id AS hostid, 
 		i.hostname AS hostname, 
