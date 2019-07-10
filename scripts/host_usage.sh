3f0af17ec245eddc36a160540e477adcbee8510b
harshal-jrvs <51926543+harshal-jrvs@users.noreply.github.com>
2019-07-10 09:57:55 -0400

    Update host_usage.sh

--- scripts/host_usage.sh
+++ scripts/host_usage.sh
@@ -8,8 +8,6 @@ db_name=$3
 user_name=$4
 password=$5
 
-
-
 get_cpu_idel(){
 	cpu_idel=$(sar -u  | head -5 | tail -1 | awk '{print $8}'| xargs)
 	echo "cpu_idel(%) =$cpu_idel"
@@ -20,14 +18,12 @@ get_cpu_kernel(){
 	echo "Cpu_kernel(%)=$cpu_kernel"
 }
 
-
 get_host_id(){
  while read p
  do
  echo $p
  done < ~/host_id
 }
-
 
 get_disk_available(){
 	disk_available=$(df /home -BM | tail -1 | awk '{print $4}'| xargs | sed s'/M//')
@@ -36,8 +32,6 @@ get_disk_io(){
 	disk_io=$(vmstat -d | tail -1 | awk '{print $10}' | xargs)
 	echo "disk_io=$disk_io"
 }
-
-
 
 #Step 1: Parse data and setup variable 
 get_cpu_idel 
@@ -52,7 +46,7 @@ mem_free=$(vmstat --unit M | tail -1 | awk '{print $4}')
 echo "timestamp=$timestamp"
 echo "mem_free=$mem_free"
 
-#step 2: Contruct insert statemtn 
+#step 2: Contruct insert statement
 insert_stmt=$(cat << END
 INSERT INTO host_usage("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io, disk_available) VALUES ('${timestamp}',${host_id},${mem_free},${cpu_idel},${cpu_kernel},${disk_io},${disk_available});
 END
@@ -63,5 +57,3 @@ echo $insert_stmt
 export PGPASSWORD=$password
 psql -h $psql_host -p $port -U $user_name -d $db_name -c "$insert_stmt"
 sleep 1
-
-
